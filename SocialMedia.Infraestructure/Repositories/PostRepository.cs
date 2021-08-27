3213822a44b83a66abc72f068c6ea2b1cdd2c688
Alexislml96 <alexislml96@gmail.com>
2021-08-26 20:53:49 -0500

    Initial commit

---
+++ SocialMedia.Infraestructure/Repositories/PostRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMedia.Core.Entities;
+using SocialMedia.Core.Interfaces;
+using SocialMedia.Infraestructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Infraestructure.Repositories
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly BasePruebaHDContext _context;
+        public PostRepository(BasePruebaHDContext context)
+        {
+            _context = context;
+        }
+        public async Task <IEnumerable<Publicacion>> GetPosts()
+        {
+            var posts = await _context.Publicacions.ToListAsync();
+            return posts;
+        }
+    }
+}
