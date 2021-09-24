c0adb0e89c33b98ed66eec9373b2088a7a06ff3f
Alexislml96 <alexislml96@gmail.com>
2021-09-24 16:18:09 -0500

    Added test code
    Test code added to try deploy to azure

---
+++ SocialMedia.Api/Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace SocialMedia.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        public string TestString()
+        {
+            return "Test CI/CD DevOps";
+        }
+    }
+}
